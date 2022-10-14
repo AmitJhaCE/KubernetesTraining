a507f0529fceae016cd7c60f42d81fbff33adedd
AmitJhaCE <amitjha@creativeentropy.in>
2022-10-14 13:11:06 +0530

    With code for host IP address

--- hello/main.go
+++ hello/main.go
@@ -4,10 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
+}
+
+//Handler
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+	localAddr := GetOutboundIP()
+	fmt.Fprintln(w, "Hello, 世界.", "Host IP address is:", localAddr)
 }
 
 func main() {
